4b6646c21bca15b0ef3f09d845663c38c2285590
root <root@ip-172-31-36-128.ap-northeast-1.compute.internal>
2020-11-19 08:34:30 +0000

    first commit

---
+++ lesson.py
@@ -0,0 +1,20 @@
+import math
+
+num = 1
+name = 'hirochan'
+is_ok = True
+
+print(num, type(num))
+print(name, type(name))
+print(is_ok, type(is_ok))
+
+print('Hi', 'Hirochan', sep=',', end='\n')
+print('Hi', 'Hirochan', sep=',', end='\n')
+
+result = math.sqrt(25)
+print(result)
+#print(help(math))
+
+x, y, z = 1, 2, 3
+print(f'a is {x}, {y}, {z}')
+
