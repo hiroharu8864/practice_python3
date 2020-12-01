3d3fd33dff842a2e6523da0dd93ef06d498d6590
hiroharu8864 <hiroharu8864@gol.com>
2020-12-01 08:51:34 +0000

    add library

--- lesson.py
+++ lesson.py
@@ -1,4 +1,7 @@
 import math
+import sys
+
+import termcolor
 
 num = 1
 name = 'hirochan'
@@ -15,6 +18,27 @@ result = math.sqrt(25)
 print(result)
 #print(help(math))
 
-x, y, z = 1, 2, 3
-print(f'a is {x}, {y}, {z}')
+is_ok = [1, 2, 3, 4 ,5]
+if is_ok:
+    print('OK!')
+else:
+    print('Not OK!')
 
+print(1 is 2)
+
+count = 0
+while count < 10:
+    print(name)
+    count += 1
+
+contents = "Today is a very hot.Today is a very hot.Today is a very hot."
+count_dict = {}
+for c in contents:
+    count_dict.setdefault(c, 0)
+    count_dict[c] += 1
+print(count_dict)
+
+print(termcolor.colored('hello', 'green'))
+print(termcolor.colored('hello', 'cyan'))
+print(termcolor.__file__)
+print(sys.path)
