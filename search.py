b8b59008dc23796fddecc67ef7138a74be2864f7
hiroharu8864 <hiroharu8864@gol.com>
2021-02-26 23:35:08 +0000

    add docstring of search.py

--- search.py
+++ search.py
@@ -1,3 +1,6 @@
+"""
+2分探索を行う関数 search()
+"""
 def search(data, target):
     start, end = 0, len(data)
     while start <= end:
@@ -10,6 +13,6 @@ def search(data, target):
             end = i - 1
     return -1
 
-data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-target = 7
+data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+target = 11
 print("要素番号{} データ{}".format(search(data, target), target))
